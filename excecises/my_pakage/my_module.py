87bd3538c5ea0a047a41fccb0f0dfcc62ed85791
Hemanth Surthani <hemanthsurthani7@gmail.com>
2025-03-27 23:49:46 +0530

    practiece up to 10th day

---
+++ excecises/my_pakage/my_module.py
@@ -0,0 +1,11 @@
+def greeting(name):
+    print(f"hello {name} ! Welcome to my data lab")
+
+
+def thanking(name):
+    print(f"Mr {name} thankyou for visiting my channel")
+
+
+channel_name = "my data lab"
+
+pi = 3.14
